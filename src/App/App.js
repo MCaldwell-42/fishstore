f727646c8c704febb81f5092aeb3e1415e271b7d
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-06-24 18:37:45 -0500

    setup

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-danger'>FISH STORE!</button>
+    </div>
+  );
+}
+
+export default App;
