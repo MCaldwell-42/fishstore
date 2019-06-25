4adb17b2832486adf91937201fcb51cf0ba2056f
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-06-24 20:28:53 -0500

    whoops

--- src/App/App.js
+++ src/App/App.js
@@ -24,6 +24,10 @@ class App extends React.Component {
     });
   }
 
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     const loadComponent = () => {
       if (this.state.authed) {
