ddbf197f811a481e89eebc4779a8e6c4cc4f2db5
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-07-01 18:14:11 -0500

    updated with Zoes Code

--- src/components/OrderRow/OrderRow.js
+++ src/components/OrderRow/OrderRow.js
@@ -1,14 +1,31 @@
+
 import React from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+
+import orderShapes from '../../helpers/propz/orderShapes';
 
 class OrderRow extends React.Component {
+  static propTypes = {
+    order: orderShapes.orderShape,
+    deleteOrder: PropTypes.func.isRequired,
+  }
+
+  deleteOrderEvent = (e) => {
+    const { order, deleteOrder } = this.props;
+    e.preventDefault();
+    deleteOrder(order.id);
+  };
+
   render() {
     const { order } = this.props;
+    const numFish = Object.values(order.fishes).reduce((a, b) => a + b);
     return (
       <tr>
-        <th>{order.id}</th>
-        <td>{order.dateTime}</td>
-        <td>5</td>
-        <td><button className={'btn btn-danger'}>X</button></td>
+        <th>{order.name}</th>
+        <td>{moment(order.dateTime).format('LLL')}</td>
+        <td>{numFish}</td>
+        <td><button className="btn btn-danger" onClick={this.deleteOrderEvent}>x</button></td>
       </tr>
     );
   }
